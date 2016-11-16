e0cc2cd39a1592523d234d364ac30df7054c3c82
aneesh k <aneesh.k@razorthink.net>
2016-11-16 13:32:14 +0530

    added java files and projects

---
+++ java files/abc.java
@@ -0,0 +1,22 @@
+class test
+{
+   public int a=2;
+   public int b=3;
+
+
+void show() {
+
+System.out.println("Value of a is"+ a);
+System.out.println("Value of b is"+ b);
+
+}
+
+
+public static void main(String args[]) {
+
+  test obj1 = new test();
+  obj1.show();
+
+}
+
+}
